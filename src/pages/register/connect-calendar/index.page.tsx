d7de492ba8804b3409aea3700605e6e53774194c
thiagocabralcorreia <thiagocabralcorreia@gmail.com>
2023-02-17 09:54:14 -0300

    Update ConnectCalendar with error message

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,15 +1,21 @@
 import { Button, Heading, MultiStep, Text } from "@ignite-ui/react";
-import { ArrowRight } from "phosphor-react";
-// import { api } from "../../../lib/axios"
-import { ConnectBox, ConnectItem } from "./styles";
-import { Container, Header } from "../styles";
 import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { ArrowRight, Check } from "phosphor-react";
+// import { api } from "../../../lib/axios"
+import { Container, Header } from "../styles";
+import { AuthError, ConnectBox, ConnectItem } from "./styles";
 
 export default function ConnectCalendar() {
   const session = useSession();
-  // async function handleRegister() {
+  const router = useRouter();
 
-  // }
+  const hasAuthError = !!router.query.error;
+  const isSignedId = session.status === "authenticated";
+
+  async function handleConnectCalendar() {
+    await signIn("google");
+  }
 
   return (
     <Container>
@@ -26,21 +32,34 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => signIn("google")}
-          >
-            Conectar
-            <ArrowRight />
-          </Button>
+          {isSignedId ? (
+            <Button size="sm" disabled>
+              Conectado
+              <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar
+              <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
 
-        {/* <Text>{JSON.stringify(session.data)}</Text> */}
+        {hasAuthError && (
+          <AuthError size="sm">
+            Falha ao se conectar ao Google, verifique se você habilitou as
+            permissões de acesso ao Google Calendar
+          </AuthError>
+        )}
 
-        <Button type="submit">
+        <Button type="submit" disabled={!isSignedId}>
           Próximo passo
           <ArrowRight />
+          {/* <Text>{JSON.stringify(session.data)}</Text> */}
         </Button>
       </ConnectBox>
     </Container>
